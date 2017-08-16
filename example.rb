94584e2d2d2eb24d12056158850e5466c03a76e0
banana <banana@eurus.cn>
2017-08-16 11:00:36 +0800

    cleanup code

--- example.rb
+++ example.rb
@@ -1,23 +1,5 @@
 require './schema_generator'
 
-sg = SchemaGenerator.new('./example.schema.txt')
-
-h = {}
-
-sg.split_into_nodes.combination(2).to_a.each do |node_a, node_b|
-  link = Link.new(node_a, node_b)
-  h["#{node_a.name}_#{node_b.name}"] = link.link?
-end
-
-File.open('test.dot', 'w+') do |writeable|
-  writeable.write("digraph g {\n")
-  writeable.write("node [shape=record,color=Red,fontname=Courier];\n")
-  writeable.write("edge [color=Blue]\n")
-  sg.split_into_nodes.each do |node|
-    writeable.write node.to_struct
-  end
-  h.select {|k,v|v == true}.each do |k,v| 
-    writeable.write "#{k.split('_')[0]} -> #{k.split('_')[1]} [dir=\"none\"]\n"
-  end
-  writeable.write("}\n")
-end
+sg = SchemaGenerator.new('./example.schema.txt', 'test.dot')
+sg.split_into_nodes
+sg.draw
--- schema_generator.rb
+++ schema_generator.rb
@@ -8,31 +8,6 @@ class String
   end
 end
 
-class SchemaGenerator
-  def initialize(filename)
-    @filename = filename
-    @nodes = []
-  end
-
-  def split_into_nodes
-    original = File.open(@filename, 'r') { |file| file.readlines }
-    blankless = original.reject{ |line| line.match(/^$/) }
-
-    @nodes = blankless.join().split("--------------------------------------------------").map do |b|
-      sources = b.lines.reject{|line| line == "\n"}
-      name = sources.first.gsub("\n",'')
-      attrs = sources[2..-1].map { |e| e.split(' ')
-      .join(':') }
-      .reject{ |s| s.include? "created_at" }
-      .reject{ |s| s.include? "updated_at"}
-      .reject{ |s| s == "id:int(11)"}
-      .concat ["#{name.underscore}_id:int(11)"]
-      .uniq
-      sn = SchemaNode.new(name, attrs)
-    end
-  end
-end
-
 class SchemaNode
   attr_accessor :name, :attrs
   def initialize(name, attrs)
@@ -60,5 +35,62 @@ class Link
     case_a = @node_b.attrs.include?("#{@node_a.name.underscore}_id:int(11)")
     case_b = @node_a.attrs.include?("#{@node_b.name.underscore}_id:int(11)")
     case_a || case_b
+  end
+end
+
+class SchemaGenerator
+  def initialize(source, target)
+    @source, @target = source, target
+    @nodes = []
+    @h = {}
+  end
+
+  def split_into_nodes
+    original = File.open(@source, 'r') { |file| file.readlines }
+    blankless = original.reject{ |line| line.match(/^$/) }
+
+    @nodes = blankless
+      .join()
+      .split("--------------------------------------------------").map do |b|
+      sources = b.lines.reject{|line| line == "\n"}
+      name = sources.first.gsub("\n",'')
+      attrs = sources[2..-1].map { |e| e.split(' ')
+      .join(':') }
+      .reject{ |s| s.include? "created_at" }
+      .reject{ |s| s.include? "updated_at"}
+      .reject{ |s| s == "id:int(11)"}
+      .concat ["#{name.underscore}_id:int(11)"]
+      .uniq
+      sn = SchemaNode.new(name, attrs)
+    end
+  end
+  
+  def draw
+    File.open(@target, 'w+') do |writeable|
+      writeable.write("digraph g {\n")
+      writeable.write("node [shape=record,color=Red,fontname=Courier];\n")
+      writeable.write("edge [color=Blue]\n")
+
+      draw_node writeable
+      draw_edge writeable
+
+      writeable.write("}\n")
+    end
+  end
+
+  private
+  def draw_node(writeable)
+    @nodes.each do |node|
+      writeable.write node.to_struct
+    end
+  end
+
+  def draw_edge(writeable)
+    @nodes.combination(2).to_a.each do |node_a, node_b|
+      link = Link.new(node_a, node_b)
+      @h["#{node_a.name}_#{node_b.name}"] = link.link?
+    end.select {|k,v|v == true}.each do |k,v| 
+      writeable.write "#{k.split('_')[0]} -> #{k.split('_')[1]} [dir=\"none\"]\n"
+    end
   end
 end
